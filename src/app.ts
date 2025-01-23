ebe4a63836af9b232060428d59e0eded911674bc
jay <ollama.jay@gmail.com>
2025-01-23 15:47:54 +0530

    hello from mac

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import coilRoute from "./routes/coilRoute";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(prettyJSON());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono! from jay ");
+});
+
+app.route("/api/coil", coilRoute);
+
+
+
+export default app;
---
+++ src/routes/coilRoute.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+
+const coilSchema = z.object({
+  name: z.string(),
+  sets: z.number(),
+  weight: z.number(),
+  delivered: z.boolean(),
+});
+
+const coilRoute = new Hono()
+  .get("/", (c) => {
+    return c.json({ message: "Hello Hono! from jay" });
+  })
+  .post("/", zValidator("json" , coilSchema) , async (c) => {
+    const data = await c.req.valid("json");
+    const coil = coilSchema.parse(data);
+    return c.json(coil);
+  });
+
+export default coilRoute;
